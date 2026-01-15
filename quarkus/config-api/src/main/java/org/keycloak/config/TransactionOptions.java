d206c63846cd83fe3f48708024f272c18ffb97a8
Garth <244253+xgp@users.noreply.github.com>
2026-01-15 11:25:09 +0100

    update to 26.5.1
    Signed-off-by: Garth <244253+xgp@users.noreply.github.com>

--- quarkus/config-api/src/main/java/org/keycloak/config/TransactionOptions.java
+++ quarkus/config-api/src/main/java/org/keycloak/config/TransactionOptions.java
@@ -19,6 +19,14 @@ public class TransactionOptions {
             .wildcardKey(TRANSACTION_XA_ENABLED_DATASOURCE.getKey())
             .build();
 
+     public static final Option<Boolean> TRANSACTION_JTA_ENABLED = new OptionBuilder<>("transaction-jta-enabled", Boolean.class)
+            .category(OptionCategory.TRANSACTION)
+            .description("Set if distributed transactions are supported. If set to false, transactions are managed by the server and can not be joined if multiple data sources are used. By default, distributed transactions are enabled and only XA data sources can be used.")
+            .buildTime(true)
+            .defaultValue(Boolean.TRUE)
+            .hidden()
+            .build();
+
     public static String getNamedTxXADatasource(String namedProperty) {
         if ("<default>".equals(namedProperty)) {
             return TRANSACTION_XA_ENABLED.getKey();
